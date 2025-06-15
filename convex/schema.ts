49738fff3ccd425b1b0f5d1ef8c693efba11deb0
Hari Lamichhane <onwithhari@gmail.com>
2025-06-15 18:44:17 +0545

    remove deleted schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,5 +1,4 @@
 import { defineSchema } from 'convex/server';
-import { attachmentsTable } from './attachments/table';
 import { chatsTable } from './chats/table';
 import { messagesTable } from './messages/table';
 import { linkingTable } from './linking/table';
@@ -8,7 +7,6 @@ import { authTables } from '@convex-dev/auth/server';
 const convexSchema = defineSchema({
   chats: chatsTable,
   messages: messagesTable,
-  attachments: attachmentsTable,
   linking: linkingTable,
   ...authTables,
 });
