e84cc4f368092a045b76b9f731efe6e0677e15e2
Hari Lamichhane <onwithhari@gmail.com>
2025-06-18 18:54:16 +0545

    fix name

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -12,7 +12,7 @@ import { Route as rootRouteImport } from './routes/__root'
 import { Route as ShareRouteRouteImport } from './routes/share/route'
 import { Route as LoginRouteRouteImport } from './routes/login/route'
 import { Route as ChatRouteRouteImport } from './routes/chat/route'
-import { Route as RouteRouteImport } from './routes/route'
+import { Route as IndexRouteImport } from './routes/index'
 import { Route as ShareChatIdRouteImport } from './routes/share/$chatId'
 import { Route as SessionSessionIdRouteImport } from './routes/session/$sessionId'
 import { Route as ChatChatIdRouteImport } from './routes/chat/$chatId'
@@ -32,7 +32,7 @@ const ChatRouteRoute = ChatRouteRouteImport.update({
   path: '/chat',
   getParentRoute: () => rootRouteImport,
 } as any)
-const RouteRoute = RouteRouteImport.update({
+const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
   getParentRoute: () => rootRouteImport,
@@ -54,7 +54,7 @@ const ChatChatIdRoute = ChatChatIdRouteImport.update({
 } as any)
 
 export interface FileRoutesByFullPath {
-  '/': typeof RouteRoute
+  '/': typeof IndexRoute
   '/chat': typeof ChatRouteRouteWithChildren
   '/login': typeof LoginRouteRoute
   '/share': typeof ShareRouteRouteWithChildren
@@ -63,7 +63,7 @@ export interface FileRoutesByFullPath {
   '/share/$chatId': typeof ShareChatIdRoute
 }
 export interface FileRoutesByTo {
-  '/': typeof RouteRoute
+  '/': typeof IndexRoute
   '/chat': typeof ChatRouteRouteWithChildren
   '/login': typeof LoginRouteRoute
   '/share': typeof ShareRouteRouteWithChildren
@@ -73,7 +73,7 @@ export interface FileRoutesByTo {
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
-  '/': typeof RouteRoute
+  '/': typeof IndexRoute
   '/chat': typeof ChatRouteRouteWithChildren
   '/login': typeof LoginRouteRoute
   '/share': typeof ShareRouteRouteWithChildren
@@ -112,7 +112,7 @@ export interface FileRouteTypes {
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
-  RouteRoute: typeof RouteRoute
+  IndexRoute: typeof IndexRoute
   ChatRouteRoute: typeof ChatRouteRouteWithChildren
   LoginRouteRoute: typeof LoginRouteRoute
   ShareRouteRoute: typeof ShareRouteRouteWithChildren
@@ -146,7 +146,7 @@ declare module '@tanstack/react-router' {
       id: '/'
       path: '/'
       fullPath: '/'
-      preLoaderRoute: typeof RouteRouteImport
+      preLoaderRoute: typeof IndexRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/share/$chatId': {
@@ -198,7 +198,7 @@ const ShareRouteRouteWithChildren = ShareRouteRoute._addFileChildren(
 )
 
 const rootRouteChildren: RootRouteChildren = {
-  RouteRoute: RouteRoute,
+  IndexRoute: IndexRoute,
   ChatRouteRoute: ChatRouteRouteWithChildren,
   LoginRouteRoute: LoginRouteRoute,
   ShareRouteRoute: ShareRouteRouteWithChildren,
